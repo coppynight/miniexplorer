97128ed79dd3ff25226e9afa19227c10e73dbbc2
coppynight <278398990@qq.com>
2026-02-12 23:23:09 +0800

    feat: simplify explore overlay and remove companion video preview

--- h5/js/main.js
+++ h5/js/main.js
@@ -1,6 +1,6 @@
-import { initRealtime } from './realtime.js?v=handsfree-11';
-import { createUI } from './ui.js?v=handsfree-11';
-import { ConversationEngine } from './engine.js?v=handsfree-11';
+import { initRealtime } from './realtime.js?v=handsfree-12';
+import { createUI } from './ui.js?v=handsfree-12';
+import { ConversationEngine } from './engine.js?v=handsfree-12';
 
 class Router {
   constructor(app) {
